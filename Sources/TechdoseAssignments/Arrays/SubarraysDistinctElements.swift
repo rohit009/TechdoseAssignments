49d210aeba129805f7af98d8014a7b6b36863802
Rohit009 <thisisrohit009@gmail.com>
2022-10-09 17:36:27 +0530

    [Array] Added full implementation for: Count of k distinct elements subarrys

--- Sources/TechdoseAssignments/Arrays/SubarraysDistinctElements.swift
+++ Sources/TechdoseAssignments/Arrays/SubarraysDistinctElements.swift
@@ -2,73 +2,44 @@
 //  Created by Rohit Patil on 05/10/22.
 import Foundation
 
+// Question: Find the count of sub-arrays with atmost k unique numbers from the given array.
+// Approach: Sliding window + Map.
 // https://www.codingninjas.com/codestudio/problems/subarrays-with-at-most-k-distinct-values_1473804
-// Find the count of sub-arrays with atmost k unique numbers from the given array.
-// Need to revisit this as the approach does not work for some test cases. Approach: Variable size sliding window.
 class SubarraysDistinctElements {
     public func numberOfSubarraysHaving(kDistinctElements: Int, in numbers: [Int]) -> Int {
         var swStart = 0
         var swEnd = 0
+        var numberFrequencies = Dictionary<Int, Int>()
         var subarraysCount = 0
-        let lastIndex = numbers.count - 1
         
-        while swStart < lastIndex || swEnd < lastIndex {
-            let subarrayHasKDistinctElements = isSubarray(
-                numbers: numbers[swStart ... swEnd],
-                withAtmost: kDistinctElements,
-                start: swStart,
-                end: swEnd
-            )
+        while swEnd < numbers.count {
+            let swEndElement = numbers[swEnd]
+            if let frequency = numberFrequencies[swEndElement] {
+                numberFrequencies[swEndElement] = frequency + 1
+            } else {
+                numberFrequencies[swEndElement] = 1
+            }
             
-            if subarrayHasKDistinctElements {
-                for _ in swStart ... swEnd {
-                    subarraysCount += 1
-                }
-                
-                if swStart == 0 && swEnd == lastIndex {
-                    break
-                }
-                
-                if swEnd < lastIndex {
-                    swEnd += 1
+            // if (numberFrequencies.count <= kDistinctElements) condition will be handled at the end of this while
+            // loop.
+            
+            while numberFrequencies.count > kDistinctElements {
+                let swStartElement = numbers[swStart]
+                if let frequency = numberFrequencies[swStartElement] {
+                    if frequency == 1 {
+                        numberFrequencies.removeValue(forKey: swStartElement)
+                    } else {
+                        numberFrequencies[swStartElement] = frequency - 1
+                    }
+                    swStart += 1
                 }
             }
             
-            if !subarrayHasKDistinctElements || swEnd == lastIndex {
-                swStart += 1
-            }
+            subarraysCount += (swEnd - swStart + 1)
+            swEnd += 1
         }
         
         return subarraysCount
     }
-    
-    private func isSubarray(
-        numbers: ArraySlice<Int>,
-        withAtmost kDistinctElements: Int,
-        start: Int,
-        end: Int
-    ) -> Bool {
-        var distinctCount = 0
-        
-        for i in start ... end {
 
-            var isDupe = false
-            for j in start ... end {
-                if j == i {
-                    continue
-                }
-                
-                if numbers[i] == numbers[j] {
-                    isDupe = true
-                    break
-                }
-            }
-
-            if !isDupe {
-                distinctCount += 1
-            }
-        }
-        
-        return distinctCount <= kDistinctElements
-    }
 }
--- Tests/TechdoseAssignmentsTests/ArraysTests/SubarraysDistinctElementsTests.swift
+++ Tests/TechdoseAssignmentsTests/ArraysTests/SubarraysDistinctElementsTests.swift
@@ -20,11 +20,36 @@ class SubarraysDistinctElementsTests: XCTestCase {
         XCTAssertEqual(output, expectedOutput)
     }
 
-    // Test case not working.
     func testSubarraysDistinctElements_Input2() {
         let numbers = [1, 2, 3, 1]
         let kDistinctElements = 3
         let expectedOutput = 10
+        
+        let output = SubarraysDistinctElements().numberOfSubarraysHaving(
+            kDistinctElements: kDistinctElements,
+            in: numbers
+        )
+        
+        XCTAssertEqual(output, expectedOutput)
+    }
+    
+    func testSubarraysDistinctElements_Input3() {
+        let numbers = [1, 2, 3, 2]
+        let kDistinctElements = 2
+        let expectedOutput = 8
+        
+        let output = SubarraysDistinctElements().numberOfSubarraysHaving(
+            kDistinctElements: kDistinctElements,
+            in: numbers
+        )
+        
+        XCTAssertEqual(output, expectedOutput)
+    }
+    
+    func testSubarraysDistinctElements_Input4() {
+        let numbers = [1, 2, 3, 4, 5]
+        let kDistinctElements = 4
+        let expectedOutput = 14
         
         let output = SubarraysDistinctElements().numberOfSubarraysHaving(
             kDistinctElements: kDistinctElements,
